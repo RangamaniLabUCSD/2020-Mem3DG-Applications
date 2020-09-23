ffa586673e0064a988897746f205a0866d28c77f
cuzhucuncheng <cuzhu@ucsd.edu>
2020-09-22 18:19:50 -0700

    sweep of area

--- param_sweep/sweep.py
+++ param_sweep/sweep.py
@@ -15,61 +15,61 @@ import numpy as np
 import concurrent.futures
 
 def worker(args):
-    kb = args[0]
+    r_H0 = args[0]
     oPath = args[1]
     # print(oPath)
     os.mkdir(oPath)
 
     # run simulation
-    pymem3dg.driver(inputMesh = 'slightlyOblate.ply',
+    pymem3dg.driver_ply(inputMesh = 'input-file\\patch.ply',
                 outputDir = oPath,
-                refMesh = 'slightlyOblate.ply',
-                radius = 10,
+                refMesh = 'input-file\\patch.ply',
+                radius = 0.9,
 
                 isTuftedLaplacian = False,
                 mollifyFactor = 1e-3,
                 isVertexShift = False,
-                isProtein = true,
+                isProtein = False,
 
-                epsilon = 8e-5,
-                Bc = 20,
-                H0 = 4,
-                sharpness = 10,
-                r_H0 = 3,
-                Vt = 0.65,
-                ptInd = 0,  
+                epsilon = 15e-5,
+                Bc = 40,
+                H0 = 40,
+                sharpness = 100,
+                r_H0 = r_H0,
+                Vt = 1,
+                ptInd = 672,  
                 Kf = 0,
                 conc = 25,
                 height = 0,
                 
-                Kb  = kb,
+                Kb  = 8.22e-5,
                 Kse = 0,     
-                Ksl = 0.01,
+                Ksl = 0.05,
                 Kst = 0,		
-                Ksg = [0.15,1],		
-                Kv  = [0.05,0.1], 	
+                Ksg = [0.1,0.1],		
+                Kv  = [5e-2,5e-2], 	
                 kt = 0, 
-                gamma = 10, 
+                gamma = 2, 
                 
-                h = 1e-3,
-                T = 1000,
+                h = 1e-5,
+                T = 50,
                 eps = 0.002,
                 closeZone = 1000,
                 increment = 0.1,
-                tSave = 10,
+                tSave = 1e-1,
                 tMollify = 100)
     
 def runSims():
     jobs = []
-    for i, kb in enumerate(np.arange(1e-5,0.1,1e-4)):
+    for i, r_H0 in enumerate(np.arange(0.05,0.2,0.025)):
         for replicate in np.arange(0,1):
             path = f'run_{i}_{replicate}/'
-            jobs.append((kb,path))
+            jobs.append((r_H0,path))
 
-    # with concurrent.futures.ProcessPoolExecutor() as executor:
-    #     results = executor.map(worker, jobs)     
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        results = executor.map(worker, jobs)     
 
-    r = process_map(worker, jobs, max_workers=4)
+    # r = process_map(worker, jobs, max_workers=4)
 
 if __name__ == "__main__":
     runSims()
