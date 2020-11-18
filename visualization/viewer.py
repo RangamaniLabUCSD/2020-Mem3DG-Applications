705b00a0d9ae2348ab4b9ad302974a11f0f3327b
cuzhucuncheng <cuzhu@ucsd.edu>
2020-11-17 16:23:25 -0800

    pure shape viewer option -s & -a

--- visualization/viewer.py
+++ visualization/viewer.py
@@ -1,37 +1,39 @@
 import sys
 import json
-import argparse
 import os
-
-# parse the config file 
-parser = argparse.ArgumentParser()
-# parser.add_argument("config", help = "configuration file (.json) used for the simulation", type = str)
-parser.add_argument("vis_file", help = "Mesh file (.ply, .obj, .etc.) used for visualization, OR \
-                    trajectory file (.nc) used for animation", type = str)
-args = parser.parse_args()
-ext = os.path.splitext(args.vis_file)[1][1:]
-# # read the config file
-# with open(args.config) as f:
-#     data = json.load(f)
-# geo = data["parameters"]["geometry"]
-# prop = data["parameters"]["properties"]
-# var = data["parameters"]["variables"]
-# inte = data["parameters"]["integration"]
-# dep = data["parameters"]["dependencies"]
-# opt = data["parameters"]["options"]
-
-# # find dependency
-# sys.path.append(dep["pyddg"])
+import optparse
 import pymem3dg
-
-# run viewer 
-if ext == "nc":
-    pymem3dg.view_animation(args.vis_file, ref_coord = 1, velocity = 1,
-                        mean_curvature = 1,  spon_curvature = 1,
-                       ext_pressure = 1, physical_pressure = 1,
-                       capillary_pressure = 1,
-                       bending_pressure = 1, line_pressure = 1, mask = 1, H_H0 = 1)
-elif ext == "ply": 
-    pymem3dg.viewer(args.vis_file, mean_curvature = 1, spon_curvature = 1,
-         ext_pressure = 1, physical_pressure = 1, capillary_pressure = 1,
-           bending_pressure = 1, line_pressure = 1)
+# parse the config file and options
+optparser = optparse.OptionParser()
+optparser.add_option('-s', '--shape', dest="shape", type="string",
+                     help="visualization file in .nc/.ply format")
+optparser.add_option('-a', '--all', dest="all", type="string",
+                     help="visualization file in .nc/.ply format")
+(options, args) = optparser.parse_args()
+# argparser = argparse.ArgumentParser()
+# parser.add_argument("config", help = "configuration file (.json) used for the simulation", type = str)
+# args = argparser.parse_args()
+if (options.shape != None):
+  ext = os.path.splitext(options.shape)[1][1:]
+  # run viewer 
+  if ext == "nc":
+      pymem3dg.view_animation(options.shape,ref_coord = 0, velocity = 0,
+                          mean_curvature = 0,  spon_curvature = 0,
+                        ext_pressure = 0, physical_pressure = 0,
+                        capillary_pressure = 0,
+                        bending_pressure = 0, line_pressure = 0, mask = 0, H_H0 = 0)
+  elif ext == "ply": 
+      pymem3dg.viewer(options.shape)
+elif(options.all != None):
+  ext = os.path.splitext(options.all)[1][1:]
+  # run viewer 
+  if ext == "nc":
+      pymem3dg.view_animation(options.all, ref_coord = 1, velocity = 1,
+                          mean_curvature = 1,  spon_curvature = 1,
+                        ext_pressure = 1, physical_pressure = 1,
+                        capillary_pressure = 1,
+                        bending_pressure = 1, line_pressure = 1, mask = 1, H_H0 = 1)
+  elif ext == "ply": 
+      pymem3dg.viewer(options.all, mean_curvature = 1, spon_curvature = 1,
+          ext_pressure = 1, physical_pressure = 1, capillary_pressure = 1,
+            bending_pressure = 1, line_pressure = 1)
