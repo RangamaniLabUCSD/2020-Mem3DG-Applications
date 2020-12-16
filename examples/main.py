c4465a2db9389d2537db00f4e0012dcf04581e13
zhucuncheng@gmail.com <cuzhu@ctlee@cortex.rangamani.ucsd.edu>
2020-12-15 21:54:19 -0800

    adjust for recent refactoring

--- examples/main.py
+++ examples/main.py
@@ -24,57 +24,54 @@ elif(options.nc != None):
 # read the config file
 with open(configFile) as f:
     data = json.load(f)
-geo = data["parameters"]["geometry"]
-prop = data["parameters"]["properties"]
-var = data["parameters"]["variables"]
-inte = data["parameters"]["integration"]
 dep = data["parameters"]["dependencies"]
+io = data["parameters"]["I/O"]
 opt = data["parameters"]["options"]
+var = data["parameters"]["variables"]
+prop = data["parameters"]["properties"]
+inte = data["parameters"]["integration"]
 
 # configure path for different platforms
-geo['refMesh'] = os.fspath(os.path.abspath(Path(geo['refMesh'])))
-geo['inputMesh'] = os.fspath(os.path.abspath(Path(geo['inputMesh'])))
-geo['outputDir'] = os.fspath(os.path.abspath(Path(geo['outputDir'])))
-geo['trajFile'] = os.fspath(os.path.abspath(Path(geo['trajFile'])))
+io['refMesh'] = os.fspath(os.path.abspath(Path(io['refMesh'])))
+io['inputMesh'] = os.fspath(os.path.abspath(Path(io['inputMesh'])))
+io['outputDir'] = os.fspath(os.path.abspath(Path(io['outputDir'])))
+io['trajFile'] = os.fspath(os.path.abspath(Path(io['trajFile'])))
 
 # make I/O directories if not exist
 cwd = os.getcwd()
-IDir = os.path.join(cwd, os.path.split(geo["inputMesh"])[0])
-ODir = os.path.join(cwd, geo["outputDir"])
+IDir = os.path.join(cwd, os.path.split(io["inputMesh"])[0])
+ODir = os.path.join(cwd, io["outputDir"])
 if not os.path.exists(IDir):
     os.mkdir(IDir)
 if not os.path.exists(ODir):
     os.mkdir(ODir)
 
 # copy the config.json & viewer to the outputDir
-shutil.copyfile(configFile, os.path.join(geo["outputDir"], "config.json"))
-shutil.copyfile(dep["viewer.py"], os.path.join(geo["outputDir"], "viewer.py"))
-shutil.copyfile(dep["plots.py"], os.path.join(geo["outputDir"], "plots.py"))
+shutil.copyfile(configFile, os.path.join(io["outputDir"], "config.json"))
+shutil.copyfile(dep["viewer.py"], os.path.join(io["outputDir"], "viewer.py"))
+shutil.copyfile(dep["plots.py"], os.path.join(io["outputDir"], "plots.py"))
 
 # create starting mesh
-if geo["generateGeometry"] == True:
-    pymem3dg.genIcosphere(nSub=geo["nSub"], path=geo["refMesh"], R=geo["R"])
-# elif geo["inputMesh"] == "UVsphere.ply":
-#     pymem3dg.genUVsphere( nSub = geo["nSub"]
-#                     , path = geo["inputMesh"])
+if io["generateGeometry"] == True:
+    pymem3dg.genIcosphere(nSub=io["nSub"], path=io["refMesh"], R=io["R"])
+# elif io["inputMesh"] == "UVsphere.ply":
+#     pymem3dg.genUVsphere( nSub = io["nSub"]
+#                     , path = io["inputMesh"])
 
 # run simulation
 if (options.ply != None):
-    pymem3dg.driver_ply(verbosity=geo["verbosity"],
-                        inputMesh=geo["inputMesh"],
-                        outputDir=geo["outputDir"],
-                        refMesh=geo["refMesh"],
-                        nSub=geo["nSub"],
-                        radius=geo["radiusOfIntegration"],
+    pymem3dg.driver_ply(verbosity=io["verbosity"],
+                        inputMesh=io["inputMesh"],
+                        outputDir=io["outputDir"],
+                        refMesh=io["refMesh"],
+                        nSub=io["nSub"],
 
                         isTuftedLaplacian=opt["isTuftedLaplacian"],
                         mollifyFactor=opt["mollifyFactor"],
                         isVertexShift=opt["isVertexShift"],
                         isProtein=opt["isProtein"],
 
-                        epsilon=var["epsilon"],
-                        Bc=var["Bc"],
-                        H0=var["H0*R"] / geo["R"],
+                        H0=var["H0*R"] / io["R"],
                         sharpness=var["sharpness"],
                         r_H0=var["r_H0"],
                         Vt=var["Vt"],
@@ -85,63 +82,64 @@ if (options.ply != None):
 
                         Kb=prop["Kb"],
                         eta = prop["eta"],
+                        Ksg=prop["Ksg"],
+                        Kv=prop["Kv"],
+                        epsilon=prop["epsilon"],
+                        Bc=prop["Bc"],
                         Kse=prop["Kse"],
                         Ksl=prop["Ksl"],
                         Kst=prop["Kst"],
-                        Ksg=prop["Ksg"],
-                        Kv=prop["Kv"],
                         kt=prop["kt"],
                         gamma=prop["gamma"],
 
+                        radius=inte["radiusOfIntegration"],
                         h=inte["h"],
                         T=inte["T"],
                         eps=inte["eps"],
-                        closeZone=inte["closeZone"],
-                        increment=inte["increment"],
                         tSave=inte["tSave"],
-                        tMollify=inte["tMollify"],
-                        errorJumpLim=inte["errorJumpLim"],
-                        integration = inte["method"])
+                        integration = inte["method"],
+                        isBacktrack = inte["options"]["isBacktrack"],
+                        rho = inte["options"]["rho"],
+                        c1 = inte["options"]["c1"])
 
 elif (options.nc != None):
-    pymem3dg.driver_nc(verbosity=geo["verbosity"],
-                       trajFile=geo["trajFile"],
-                       startingFrame=geo["startingFrame"],
-                       outputDir=geo["outputDir"],
-                       radius=geo["radiusOfIntegration"],
+    pymem3dg.driver_nc( verbosity=io["verbosity"],
+                        trajFile=io["trajFile"],
+                        startingFrame=io["startingFrame"],
+                        outputDir=io["outputDir"],
 
-                       isTuftedLaplacian=opt["isTuftedLaplacian"],
-                       mollifyFactor=opt["mollifyFactor"],
-                       isVertexShift=opt["isVertexShift"],
-                       isProtein=opt["isProtein"],
+                        isTuftedLaplacian=opt["isTuftedLaplacian"],
+                        mollifyFactor=opt["mollifyFactor"],
+                        isVertexShift=opt["isVertexShift"],
+                        isProtein=opt["isProtein"],
 
-                       epsilon=var["epsilon"],
-                       Bc=var["Bc"],
-                       H0=var["H0*R"] / geo["R"],
-                       sharpness=var["sharpness"],
-                       r_H0=var["r_H0"],
-                       Vt=var["Vt"],
-                       pt=var["pt"],
-                       Kf=var["Kf"],
-                       conc=var["conc"],
-                       height=var["height"],
+                        H0=var["H0*R"] / io["R"],
+                        sharpness=var["sharpness"],
+                        r_H0=var["r_H0"],
+                        Vt=var["Vt"],
+                        pt=var["pt"],
+                        Kf=var["Kf"],
+                        conc=var["conc"],
+                        height=var["height"],
 
-                       Kb=prop["Kb"],
-                       eta = prop["eta"],
-                       Kse=prop["Kse"],
-                       Ksl=prop["Ksl"],
-                       Kst=prop["Kst"],
-                       Ksg=prop["Ksg"],
-                       Kv=prop["Kv"],
-                       kt=prop["kt"],
-                       gamma=prop["gamma"],
+                        Kb=prop["Kb"],
+                        eta = prop["eta"],
+                        Ksg=prop["Ksg"],
+                        Kv=prop["Kv"],
+                        epsilon=prop["epsilon"],
+                        Bc=prop["Bc"],
+                        Kse=prop["Kse"],
+                        Ksl=prop["Ksl"],
+                        Kst=prop["Kst"],
+                        kt=prop["kt"],
+                        gamma=prop["gamma"],
 
-                       h=inte["h"],
-                       T=inte["T"],
-                       eps=inte["eps"],
-                       closeZone=inte["closeZone"],
-                       increment=inte["increment"],
-                       tSave=inte["tSave"],
-                       tMollify=inte["tMollify"],
-                       errorJumpLim=inte["errorJumpLim"],
-                       integration = inte["method"])
+                        radius=inte["radiusOfIntegration"],
+                        h=inte["h"],
+                        T=inte["T"],
+                        eps=inte["eps"],
+                        tSave=inte["tSave"],
+                        integration = inte["method"],
+                        isBacktrack = inte["options"]["isBacktrack"],
+                        rho = inte["options"]["rho"],
+                        c1 = inte["options"]["c1"])
