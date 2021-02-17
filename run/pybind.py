73114611c5512f084b7c0b93bc1ad6a408aba2de
cuzhucuncheng <cuzhu@ucsd.edu>
2021-02-16 16:21:29 -0800

    streamlined pybind script

--- run/pybind.py
+++ run/pybind.py
@@ -1,58 +1,66 @@
 import pymem3dg
 
-Kb = 8.22e-5
-H0 = 0
-sharpness = 0
-r_H0 = [-1, -1]
-Ksg = 1
-Kst = 7
-Ksl = 0
-Kse = 0
-epsilon = -1
-Bc = -1
-gamma = 0
-Kv = 0.1
-Vt = -1
-Pam = 0.3
-Kf = 0
-conc = -1
-height = 0
-radius = 100000
-temp = 0
-eta = 0
-pt = [0, 0, 0]
+# inputMesh = "/home/cuzhu/2020-Mem3DG-Applications/run/input-file/slightlyOblate.ply"
+# refMesh = "/home/cuzhu/2020-Mem3DG-Applications/run/input-file/slightlyOblate.ply"
+# outputDir = "/home/cuzhu/2020-Mem3DG-Applications/results/vesicle/testAPI"
+inputMesh = "C://Users//Kieran//Dev//2020-Mem3DG-Applications//run//input-file//slightlyOblate.ply"
+refMesh =  "C://Users//Kieran//Dev//2020-Mem3DG-Applications//run//input-file//slightlyOblate.ply"
+outputDir = "C://Users//Kieran//Dev//2020-Mem3DG-Applications//results//vesicle//testAPI"
+
+isProtein = False
+isVertexShift = False
+isReducedVolume = False
+isLocalCurvature = False
+
+nSub = 1
+
+p = pymem3dg.Parameters();
+p.Kb = 8.22e-5
+p.H0 = 0
+p.sharpness = 0
+p.r_H0 = [-1, -1]
+p.Ksg = 1
+p.Kst = 7
+p.Ksl = 0
+p.Kse = 0
+p.epsilon = -1
+p.Bc = -1
+p.Kv = 0.1
+p.Vt = -1
+p.cam = 0.3
+p.Kf = 0
+p.conc = -1
+p.height = 0
+p.radius = 100000
+p.gamma = 1
+p.temp = 321
+p.eta = 0
+p.pt = [0, 0, 0]
+p.lambdaSG = 0
+p.lambdaV = 0
 
 h = 0.0001
 T = 10
 eps = 1e-6
 tSave = 1
 isBacktrack = True
-isAdaptiveStep = True
-
-sigma = (2 * gamma * 1.380649e-8 * temp / h)**0.5
-
-isProtein = False
-isVertexShift = False
-isReducedVolume = False
-isLocalCurvature = False
 rho = 0.5
 c1 = 0
-p = pymem3dg.Parameters(Kb,    H0,  sharpness, r_H0, Ksg,    Kst,   Ksl, Kse,
-                        Kv,    eta, epsilon,   Bc,   gamma,  Vt,    Pam, temp,
-                        sigma, pt,  Kf,        conc, height, radius, 0, 0)
-inputMesh = "/home/cuzhu/2020-Mem3DG-Applications/run/input-file/slightlyOblate.ply"
-refMesh = "/home/cuzhu/2020-Mem3DG-Applications/run/input-file/slightlyOblate.ply"
-outputDir = "/home/cuzhu/2020-Mem3DG-Applications/results/vesicle/testAPI"
-f = pymem3dg.System(inputMesh, refMesh, 1, p, isReducedVolume,
-                    isProtein, isLocalCurvature, isVertexShift)
+isAdaptiveStep = True
+verbosity = 3
+
+p.sigma = (2 * p.gamma * 1.380649e-8 * p.temp / h)**0.5
+
+f = pymem3dg.System(inputMesh, refMesh, nSub, p, isReducedVolume, isProtein, isLocalCurvature, isVertexShift)
+
 f.computeFreeEnergy()
-# print(f.E.totalE)
-# print(f.computeChemicalPotential())
-# print(f.computeBendingPressure())
-# print(f.P.r_H0)
-f.computeBendingPressure()
-print(f.getBendingPressure())
-print(f.getProteinDensity())
-print(f.getVertexPositionMatrix())
-pymem3dg.euler(f, h, T, tSave, 0, 3, outputDir, isBacktrack, rho, c1, isAdaptiveStep)
-# pymem3dg.velocityVerlet(f, h, T, tSave, 0, 3, isAdaptiveStep,outputDir)
+f.computeAllForces()
+
+fb = f.getBendingPressure()
+totalEnergy = f.E.totalE
+
+f.visualize()
+
+# pymem3dg.euler(f, h, T, tSave, eps, verbosity, outputDir, isBacktrack, rho, c1, isAdaptiveStep)
+pymem3dg.velocityVerlet(f, h, T, tSave, 0, 3, isAdaptiveStep,outputDir)
+ 
