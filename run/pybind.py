f4dd038fdf51bc4f0bad963bb88e171154eb7609
cuzhucuncheng <cuzhu@ucsd.edu>
2021-02-20 16:38:49 -0800

    update pybind

--- run/pybind.py
+++ run/pybind.py
@@ -10,57 +10,85 @@ outputDir = "C://Users//Kieran//Dev//2020-Mem3DG-Applications//results//vesicle/
 isProtein = False
 isVertexShift = False
 isReducedVolume = False
-isLocalCurvature = False
+isLocalCurvature = True
 
 nSub = 1
 
-p = pymem3dg.Parameters();
+p = pymem3dg.Parameters()
 p.Kb = 8.22e-5
-p.H0 = 0
-p.sharpness = 0
-p.r_H0 = [-1, -1]
+p.H0 = 3
+p.sharpness = 50
+p.r_H0 = [1, 1]
+p.eta = 0.002
 p.Ksg = 1
-p.Kst = 7
+p.Kst = 0.1
 p.Ksl = 0
 p.Kse = 0
 p.epsilon = -1
 p.Bc = -1
-p.Kv = 0.1
+p.Kv = 0
 p.Vt = -1
 p.cam = 0.3
 p.Kf = 0
 p.conc = -1
 p.height = 0
 p.radius = 100000
-p.gamma = 1
-p.temp = 321
-p.eta = 0
+p.gamma = 0
+p.temp = 0
 p.pt = [0, 0, 0]
 p.lambdaSG = 0
 p.lambdaV = 0
 
-h = 0.0001
-T = 10
+h = 0.005
+T = 1000
 eps = 1e-6
-tSave = 1
+tSave = 10
 isBacktrack = True
 rho = 0.5
 c1 = 0
 isAdaptiveStep = True
 verbosity = 3
+isAugmentedLagrangian = False
 
 p.sigma = (2 * p.gamma * 1.380649e-8 * p.temp / h)**0.5
 
-f = pymem3dg.System(inputMesh, refMesh, nSub, p, isReducedVolume, isProtein, isLocalCurvature, isVertexShift)
+g = pymem3dg.System(inputMesh, refMesh, nSub, p, isReducedVolume,
+                    isProtein, isLocalCurvature, isVertexShift)
 
-f.computeFreeEnergy()
-f.computeAllForces()
+g.computeFreeEnergy()
+print(g.E.totalE)
+print(g.E.lE)
+g.computeAllForces()
+print(g.getLineTensionPressure())
+# g.visualize()
 
 fb = f.getBendingPressure()
 totalEnergy = f.E.totalE
 
-f.visualize()
+vv = pymem3dg.VelocityVerlet(f, h, isAdaptiveStep, T,
+                             tSave, eps, outputDir, "/traj3.nc", verbosity)
+vv.integrate()
 
-# pymem3dg.euler(f, h, T, tSave, eps, verbosity, outputDir, isBacktrack, rho, c1, isAdaptiveStep)
-pymem3dg.velocityVerlet(f, h, T, tSave, 0, 3, isAdaptiveStep,outputDir)
- 
+fe = pymem3dg.Euler(g, h, isAdaptiveStep, T,
+                    tSave, eps, outputDir, "/traj3.nc", verbosity, isBacktrack, rho, c1)
+fe.integrate();
+
+pymem3dg.animation_nc(fileName=outputDir + "/traj3.nc", transparency=0.5, angle=0,
+                              fov=50, edgeWidth=2, ref_coord=True, velocity=True,
+                              mean_curvature=True, gauss_curvature=True, spon_curvature=True,
+                              ext_pressure=True, physical_pressure=True,
+                              capillary_pressure=True, inside_pressure=True,
+                              bending_pressure=True, line_pressure=True, mask=True, H_H0=True)
+fe.step(1000)
+g.visualize()
+
+cg = pymem3dg.ConjugateGradient(f, h, isAdaptiveStep, T,
+                                tSave, eps, outputDir, "/traj.nc", verbosity, isBacktrack, rho, c1, 0.01, isAugmentedLagrangian)
+cg.integrate()
+print(f.E.potE)
+cg.status()
+cg.saveData()
+cg.march()
+print(f.E.potE)
+cg.step(100)
+print(f.E.potE)
