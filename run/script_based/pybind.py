6db539a64b54b6aa003cc0e65ba13d1d625e9cfc
cuncheng zhu <zhucuncheng@gmail.com>
2021-08-28 14:18:56 -0700

    fix: accomodate API change in mem3dg WIP

--- run/script_based/pybind.py
+++ run/script_based/pybind.py
@@ -5,11 +5,11 @@ import numpy as np
 #                 Initialize pathes                #
 ####################################################
 ### Linux ####
-inputMesh = "/home/cuzhu/2020-Mem3DG-Applications/run/input-file/boundarySpine.ply"
-# inputMesh = "/home/cuzhu/2020-Mem3DG-Applications/results/vesicle/OneSpine.ply"
+# inputMesh = "/home/cuzhu/2020-Mem3DG-Applications/run/input-file/patch.ply"
+# inputMesh = "/home/cuzhu/2020-Mem3DG-Applications/results/temp2/frame1.ply"
 # inputMesh = "/home/cuzhu/2020-Mem3DG-Applications/results/vesicle/frame21.ply"
 # refMesh = "/home/cuzhu/2020-Mem3DG-Applications/run/input-file/patch.ply"
-outputDir = "/home/cuzhu/2020-Mem3DG-Applications/results/vesicle"
+outputDir = "/home/cuzhu/2020-Mem3DG-Applications/results/temp2"
 
 ### Windows ####
 # inputMesh = "C://Users//Kieran//Dev//2020-Mem3DG-Applications//run//input-file//patch.ply"
@@ -26,100 +26,93 @@ outputDir = "/home/cuzhu/2020-Mem3DG-Applications/results/vesicle"
 #                 Initialize Meshes                #
 ####################################################
 # patFace, patVertex = dg.getHexagon(1, 4)
-# icoFace, icoVertex = dg.getIcosphere(1, 3)
+icoFace, icoVertex = dg.getIcosphere(1, 3)
 # tetFace, tetVertex = dg.getTetrahedron()
 # diaFace, diaVertex = dg.getDiamond(3.14/3)
-# cyFace, cyVertex = dg.getCylinder(1, 16, 60, 10.4, 0.1)
-soupFace, soupVertex = dg.processSoup(inputMesh)
-####################################################
-#        System: Options and Parameters            #
-####################################################
-o = dg.Options()
-o.isReducedVolume = False
-o.isConstantOsmoticPressure = True
-o.isConstantSurfaceTension = True
-o.isProteinVariation = True
-o.isShapeVariation = False
-o.isFloatVertex = True
-o.shapeBoundaryCondition = "fixed"
-o.proteinBoundaryCondition = "pin"
+# cyFace, cyVertex = dg.getCylinder(1, 16, 60, 7.5, 0)
+# soupFace, soupVertex = dg.processSoup(inputMesh)
 
-o.isEdgeFlip = False
-o.isSplitEdge = False
-o.isCollapseEdge = False
-o.isVertexShift = False
+####################################################
+#                 Parameters                       #
+####################################################
+p = dg.Parameters()
+p.proteinMobility = 1
+p.temperature = 0
+p.point.pt = [0, 0, 1]            
+p.point.isFloatVertex = True
+p.proteinDistribution.protein0 = [0.1]
+p.boundary.shapeBoundaryCondition = "none"
+p.boundary.proteinBoundaryCondition = "none"
+p.variation.isProteinVariation = True
+p.variation.isShapeVariation = True
+p.variation.radius = -1
+p.bending.Kb = 8.22e-5
+p.bending.Kbc = 0  # 8.22e-4 #DEFINITION OF LARGE AND SMALL VALUE
+p.bending.H0c = 10
+p.tension.isConstantSurfaceTension = False
+p.tension.Ksg = 1
+p.tension.A_res = 0
+p.tension.At = -1
+p.tension.lambdaSG = 0
+p.adsorption.epsilon = -1e-3
+p.osmotic.isPreferredVolume = True
+p.osmotic.isConstantOsmoticPressure = False
+p.osmotic.Kv = 0.05
+p.osmotic.V_res = 0
+p.osmotic.n = 1
+p.osmotic.Vt = 0.7 * 4.15889  # 1 * 4 * 3.1416 / 3
+p.osmotic.cam = -1
+p.osmotic.lambdaV = 0
+p.dirichlet.eta = 0.1
+p.dpd.gamma = 0
+p.external.Kf = 0
+p.external.conc = -1
+p.external.height = 0
 
+####################################################
+#                 Mesh processor                   #
+####################################################
+mP = dg.MeshProcessor()
+mP.meshMutator.shiftVertex = False
+mP.meshMutator.flipNonDelaunay = True
+# mP.meshMutator.splitLarge = True
+mP.meshMutator.splitFat = True
+mP.meshMutator.splitSkinnyDelaunay = True
+# mP.meshMutator.splitCurved = True
+# mP.meshMutator.curvTol = 0.005
+mP.meshMutator.collapseSkinny = True
+# mP.meshRegularizer.Kst = 0.1 # 2e-6
+# mP.meshRegularizer.Ksl = 0
+# mP.meshRegularizer.Kse = 0
+# mP.meshRegularizer.readReferenceData(icoFace, icoVertex, 0)
+
+
+####################################################
+#                 System                           #
+####################################################
 nSub = 0
 isContinue = False
-p = dg.Parameters()
-### general ###
-p.pt = [0, 0, 1]
-p.protein0 = [0.1]
-### bending ###
-p.Kb = 8.22e-5
-p.Kbc = 0 # 8.22e-4 #DEFINITION OF LARGE AND SMALL VALUE
-p.H0c = 10
-### surface tension ###
-p.Ksg = 0
-p.A_res = 0 
-p.epsilon = 0
-### osmotic force ###
-p.Kv = 0
-p.V_res = 0
-p.Vt = -1
-p.cam = -1
-### protein binding ###
-p.Bc = 1
-### line tension ###
-p.eta = 0.01
-### DPD ###
-p.gamma = 0
-p.temp = 0
-### regularization ###
-p.Kst = 0  # 2e-6
-p.Ksl = 0
-p.Kse = 0
-### Rarely used ###
-p.Kf = 0
-p.conc = -1
-p.height = 0
-p.radius = -1
-p.lambdaSG = 0
-p.lambdaV = 0
-
 # g = dg.System(inputMesh, nSub)
-# g = dg.System(inputMesh, p, o, nSub, isContinue)
-g = dg.System(soupFace, soupVertex, p, o, nSub)
-# g = dg.System(icoFace, icoVertex, p, o, nSub)
-# g = dg.System(patFace, patVertex, p, o, nSub)
-# g = dg.System(diaFace, diaVertex, diaVertex, nSub, p, o)
-# g = dg.System(trajFile, -1, nSub, False, p, o)
-# g = dg.System(cyFace, cyVertex, cyVertex, nSub, p, o)
-g.meshMutator.flipNonDelaunay = True
-g.meshMutator.splitLarge = True
-g.meshMutator.splitFat = True
-g.meshMutator.splitSkinnyDelaunay = True
-g.meshMutator.splitCurved = True
-g.meshMutator.curvTol = 0.003
-g.meshMutator.collapseSkinny = True
-g.computeFreeEnergy()
-g.computePhysicalForces()
-g.saveRichData("stress.ply")
+# g = dg.System(inputMesh, p, nSub, isContinue)
+# g = dg.System(soupFace, soupVertex, p, nSub)
+g = dg.System(icoFace, icoVertex, p, mP, nSub)
+# g = dg.System(patFace, patVertex, p, nSub)
+# g = dg.System(diaFace, diaVertex, diaVertex, nSub, p)
+# g = dg.System(trajFile, -1, nSub, False, p)
+# g = dg.System(cyFace, cyVertex, p, nSub)
+# g.computeFreeEnergy()
+# g.computePhysicalForces()
 # dg.visualize(g)
 
 ###################################################
 #          Time integration / Optimization
 ####################################################
-### options ###
-isBacktrack = True
-isAdaptiveStep = False
-isAugmentedLagrangian = False
 ### parameters ###
-h = 30
+h = 1
 T = 10000000
 eps = 1e-6
-tSave = 500
-rho = 0.001
+tSave = 300
+rho = 0.99
 c1 = 0.0001
 verbosity = 3
 restartNum = 3
@@ -134,9 +127,8 @@ restartNum = 3
 
 fe = dg.Euler(g, h, T, tSave, eps, outputDir)
 # fe.tUpdateGeodesics = 50
-fe.isJustGeometryPly = False
-fe.tProcessMesh = 1
-fe.verbosity = 5
+fe.tProcessMesh = 300
+# fe.isBacktrack = False
 fe.isAdaptiveStep = False
 fe.integrate()
 
